e0d253acd406b1f4b98abac82f62c9736ae00306
Aanisha Bhattacharyya <43934116+Aanisha@users.noreply.github.com>
2019-09-29 20:05:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var fs=require('fs');
+var data=fs.readFileSync('chemistry.json');
+var elements=JSON.parse(data);
+const express = require("express");
+const app = express();
+
+app.listen(5000, () => console.log("Server Start at 5000 Port"));
+app.use(express.static('public'));
+app.get('/elements',alldata);
+function alldata(request,response)
+{
+    response.send(elements);
+}
+app.get('/elements/:element/',searchElement);
+function searchElement(request,response)
+{
+	var word=request.params.element;
+	console.log(word);
+	//console.log(elements[word]);
+	if(elements[word])
+	{
+		var reply=elements[word];
+		
+	}
+	else
+	{
+		var reply={
+			status:"Not Found"
+		}
+	}
+    console.log(reply.boil);
+	response.send(reply);
+
+}

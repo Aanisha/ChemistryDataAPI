c8107c283dbf407187df35694751ffd795837f6a
mister nobody <misterybodon@gmail.com>
2020-09-21 02:26:19 +0100

    fixed typos

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-//var fs=require('fs');
-//var data=fs.readFileSync('chemistry.json');
-//var elements=JSON.parse(data);
+//load express module
 const express = require("express");
 const app = express();
-//const cors=require('cors');
-const port=process.env.PORT|5000
+
+//bring in the data
 const elements = require("./chemistry.json")
+
+//define a more robust port variable
+const port=process.env.PORT|5000
 app.listen(port, () => console.log(`Server Start at port ${port}`));
 
 app.use(express.static('public'));
-//app.use(cors());
-app.get('/elements', (req, res) => response.send(elements))
+
+app.get('/elements', (req, res) => res.json(elements))
 
 app.get('/elements/:element/',searchElement);
 function searchElement(request,response)
 {
-	const word=request.params.element;
+	let word=req.params.element;
 	word=word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
 	console.log(word);
 	//console.log(elements[word]);
-  const reply = elements[word]? 
-    elements[word]: { status:"Not Found" }
+  const reply = elements[word] ? 
+    elements[word]: 
+    { status:"Not Found" } 
   console.log(reply.boil);
-	response.send(reply);
-
+	res.send(reply);
 }
 

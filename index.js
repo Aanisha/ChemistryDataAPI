90893c6e3c65c8acef51c71c60af47df13707c8c
mister nobody <misterybodon@gmail.com>
2020-09-21 02:21:30 +0100

    ES6, port variable, removed modules

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-var fs=require('fs');
-var data=fs.readFileSync('chemistry.json');
-var elements=JSON.parse(data);
+//var fs=require('fs');
+//var data=fs.readFileSync('chemistry.json');
+//var elements=JSON.parse(data);
 const express = require("express");
 const app = express();
-const cors=require('cors');
-
-app.listen(process.env.PORT, () => console.log("Server Start at 5000 Port"));
+//const cors=require('cors');
+const port=process.env.PORT|5000
+const elements = require("./chemistry.json")
+app.listen(port, () => console.log(`Server Start at port ${port}`));
 
 app.use(express.static('public'));
-app.use(cors());
-app.get('/elements',alldata);
-function alldata(request,response)
-{
-    response.send(elements);
-}
+//app.use(cors());
+app.get('/elements', (req, res) => response.send(elements))
+
 app.get('/elements/:element/',searchElement);
 function searchElement(request,response)
 {
-	var word=request.params.element;
+	const word=request.params.element;
 	word=word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
 	console.log(word);
 	//console.log(elements[word]);
-	if(elements[word])
-	{
-		var reply=elements[word];
-		
-	}
-	else
-	{
-		var reply={
-			status:"Not Found"
-		}
-	}
-    console.log(reply.boil);
+  const reply = elements[word]? 
+    elements[word]: { status:"Not Found" }
+  console.log(reply.boil);
 	response.send(reply);
 
 }
